952d46f743cae950e41921042fa4725a855564a3
João Vitor Victorio <joaovitorvictorio@gmail.com>
2024-07-22 22:53:02 -0300

    Creating Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,24 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
 function Home() {
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+        async function loadFilmes() {
+            const response = await api.get('/movie/now_playing', {
+                params: {
+                    api_key: '078d0a638a1a05ed7822bf6005d9bda4',
+                    language: 'pt-br',
+                    page: 1,
+                }
+            })
+            console.log(response.data.results);
+        }
+        loadFilmes();
+    }, [])
+
+
     return (
         <h1>Bem-vindo a home</h1>
     )
